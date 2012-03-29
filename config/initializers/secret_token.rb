6f416e03e4af35dad9dd82ae4d40fc95e973f0ee
sandeep <sandeep@idifysolutions.com>
2012-03-29 17:26:29 +0530

    first update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCrud::Application.config.secret_token = '16109e988dfbd2b626018060aa9267471608d9fa54c3d3ce7853f4173b02da08797ec00d14a1d7226703aeb5386a91d0814aadb7d1291a2238920f8b49263cee'
